1931a7e4eca2a9323b6669377d4ae477c514555d
khalid <khalidbinalikhan@gmail.com>
2023-03-03 12:29:06 +0500

    First Commit

---
+++ chatbot/app/secret_key.py
@@ -0,0 +1,6 @@
+#Go to the OpenAI website and create an account there also,
+#make your own API key , which the OpenAI will give you for free. 
+#Place that KEY between the empty strings
+
+
+API_KEY = ''
